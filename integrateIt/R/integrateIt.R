ed1a078ab802e802fd524718983274609298e1c1
Alex Avery <a.d.avery@wustl.edu>
2022-04-04 00:53:48 -0500

    latest updates to setGeneric

--- integrateIt/R/integrateIt.R
+++ integrateIt/R/integrateIt.R
@@ -2,28 +2,29 @@
 
 setGeneric(
   name = "integrateIt",
-  def = function(fun, x, a, b, n, rule){
+  def = function(fun, x, a, b, rule){
              standardGeneric("integrateIt")
            }
   )
 
 setMethod(
   "integrateIt",
-  def = function(fun, x, a, b, n, rule){
+  def = function(fun, x, a, b, rule){
     #get x_values vector
     x_values <- x[x >= a & x <= b]
     #get y_values vector
     y_values <- sapply(x_values, fun)
+    #get n value
+    n <- length(x_values)
     #create h
     h <- (b - a) / n
     if(rule == "Trapezoid" | rule == "trapezoid"){
       #Trapezoidal Rule
-      t_result <- (h / 2) * (y[1] + 2(sum(y[2:(n-1)])) + y[n])
+      t_result <- (h / 2) * (y_values[1] + 2(sum(y_values[2:(n - 1)])) + y_values[n])
     }
     if(rule == "Simpson" | rule == "simpson"){
       #Simpson Rule
-      i <- 2
-      s_result <- (h / 3) * (y[1] + 4(sum(y[seq(from = 2, to = n, by = 2)])) + 2(sum(y[seq(from = 3, to = n, by = 2)])) + y[n])
+      s_result <- (h / 3) * (y_values[1] + 4(sum(y_values[seq(from = 2, to = (n-1), by = 2)])) + 2(sum(y_values[seq(from= 3, to = (n-1), by = 2)])) + y_values[n])
     }
     else{
       stop("Please use either Trapezoid or Simpson rule.")
@@ -32,5 +33,4 @@ setMethod(
 )
 
 
-test <- integrateIt(fun = (x^2), x = (1:80), a = 1, b = 80, n = 40, rule = "Trapezoid")
 
