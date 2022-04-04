fd4165edd348c58044b6db726dbc2fe059fc367e
Alex Avery <a.d.avery@wustl.edu>
2022-04-03 21:31:40 -0500

    progress to setMethod for integrateIt

--- integrateIt/R/integrateIt.R
+++ integrateIt/R/integrateIt.R
@@ -2,7 +2,23 @@
 
 setGeneric(
   name = "integrateIt",
-  def = function(x, y, a, b, rule){
+  def = function(x, y, a, b, n, rule){
              standardGeneric("integrateIt")
            }
   )
+
+setMethod(
+  "integrateIt",
+  def = function(x, y, a, b, n, rule){
+    #create interval for integration
+    interval <- seq(a, b, length.out = n)
+    #create h
+    h <- (b - a) / n
+    if(rule == "Trapezoid" | rule == "trapezoid"){
+      #Trapezoidal Rule
+      t_result <- (h / 2) * ()
+    }
+  }
+)
+
+
