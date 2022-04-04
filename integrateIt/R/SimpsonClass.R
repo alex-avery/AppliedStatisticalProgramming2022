032270f86f879d89361efab940710b60828a1bff
Alex Avery <a.d.avery@wustl.edu>
2022-04-03 21:00:22 -0500

    setClass added to SimpsonClass

---
+++ integrateIt/R/SimpsonClass.R
@@ -0,0 +1,17 @@
+setClass(
+  Class = "Simpson",
+  representation = representation(
+    x = "numeric",
+    y = "numeric",
+    a = "numeric",
+    b = "numeric",
+    n = "numeric"
+  ),
+  prototype = prototype(
+    x = NA_real_,
+    y = NA_real_,
+    a = NA_real_,
+    b = NA_real_,
+    n = NA_real_
+  )
+)
