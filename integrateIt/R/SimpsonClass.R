afdb74da853bb03b4c45bf0c8521c4755c39575a
Alex Avery <a.d.avery@wustl.edu>
2022-04-03 21:01:31 -0500

    setValidity added to SimpsonClass

--- integrateIt/R/SimpsonClass.R
+++ integrateIt/R/SimpsonClass.R
@@ -15,3 +15,22 @@ setClass(
     n = NA_real_
   )
 )
+
+setValidity(
+  "Simpson",
+  function(object){
+    #test that x and y are the same length
+    test_xy_length <- length(object@x) == length(object@y)
+    #test that n is equal to the length of vector x
+    test_n_length <- n == length(object@x)
+    #test that a and b are in vector x
+    test_ab <- (object@a %in% object@x) & (object@b %in% object@x)
+    if(!test_xy_length){
+      stop("The vectors x and y are not of the same length.")}
+    if(!test_n_length){
+      stop("n is not the correct length.")}
+    if(!test_ab){
+      stop("a and/or b is not contained in x.")
+    }
+  }
+)
