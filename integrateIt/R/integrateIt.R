aec1e8fa3c1a9f0ddaf35947ca509f6905a15933
Alex Avery <a.d.avery@wustl.edu>
2022-04-03 21:08:43 -0500

    generic for integrateIt

--- integrateIt/R/integrateIt.R
+++ integrateIt/R/integrateIt.R
@@ -6,12 +6,3 @@ setGeneric(
              standardGeneric("integrateIt")
            }
   )
-
-setMethod("integrateIt",
-          definition = function(x, y, a, b, rule){
-            interval <- 
-          }
-          )
-
-Takes four arguments: a vector of values(x), a vector of evaluated values(f(x) =y),starting/ending values(a, b), and aRuleargument that can be either "Trapezoid" or "Simpson".3
-ii.Have three outputs: an object of classTrapezoidor classSimpson, the values ofxandy, andthe resultiii. Both classes should have validation methods that include a few appropriate testsiv.you will need to create aninitializefunction for each class, which will be used internallybyintegrateI
