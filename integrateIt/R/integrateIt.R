d09515794d019967cb339577ca801eba1b272b89
Alex Avery <a.d.avery@wustl.edu>
2022-04-04 02:28:50 -0500

    fixed typo errors in function

--- integrateIt/R/integrateIt.R
+++ integrateIt/R/integrateIt.R
@@ -20,17 +20,27 @@ setMethod(
     h <- (b - a) / n
     if(rule == "Trapezoid" | rule == "trapezoid"){
       #Trapezoidal Rule
-      t_result <- (h / 2) * (y_values[1] + 2(sum(y_values[2:(n - 1)])) + y_values[n])
+      t_result <- (h / 2) * (y_values[1] + 2 * (sum(y_values[2:(n - 1)])) + y_values[n])
+      return(new(
+            "Trapezoid",
+             x = x_values,
+             y = y_values,
+             result = t_result)
+             )
     }
     if(rule == "Simpson" | rule == "simpson"){
       #Simpson Rule
-      s_result <- (h / 3) * (y_values[1] + 4(sum(y_values[seq(from = 2, to = (n-1), by = 2)])) + 2(sum(y_values[seq(from= 3, to = (n-1), by = 2)])) + y_values[n])
+      s_result <- (h / 3) * (y_values[1] + 4 * (sum(y_values[seq(from = 2, to = (n-1), by = 2)])) + 2 * (sum(y_values[seq(from= 3, to = (n-1), by = 2)])) + y_values[n])
+      return(new(
+        "Simpson",
+        x = x_values,
+        y = y_values,
+        result = s_result)
+        )
     }
     else{
       stop("Please use either Trapezoid or Simpson rule.")
     }
   }
 )
-
-
 
