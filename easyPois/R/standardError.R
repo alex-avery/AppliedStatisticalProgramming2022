b7d20f06101e1a847301c08b0149d57c4431ebf8
Alex Avery <a.d.avery@wustl.edu>
2022-04-12 16:15:47 -0500

    create standardError function (no documentation)

---
+++ easyPois/R/standardError.R
@@ -0,0 +1,20 @@
+standardError <- function(y, SEtype, B){
+  #get n
+  n <- length(y)
+  if(SEtype == "basic"){
+    SE <- sqrt(mle(y) / n)
+  }
+  if(SEtyoe == "bootstrap"){
+    #create matrix of samples
+    samples <- matrix(unlist(lapply(c(1:B), FUN = sample(y, n, replace = TRUE))), nrow = n, ncol = B)
+    #calculate MLE for each sample
+    mleSamples <- apply(samples, MARGIN = 2, FUN = mle)
+    #find standard deviation
+    mleSD <- sd(mleSamples)
+    SE <- mleSD
+  }
+  else{
+    stop("Invalid SEtype argument. Please use basic or bootstrap.")
+  }
+  return(SE)
+}
