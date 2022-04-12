109f3d4a894257d65d71fb5689d386fc5041ff4e
Alex Avery <a.d.avery@wustl.edu>
2022-04-12 15:25:30 -0500

    add logLik function (no documentation)

---
+++ easyPois/R/logLik.R
@@ -0,0 +1,11 @@
+logLik <- function(y, lambda){
+  #getting the number of observations
+  n <- length(y)
+  #first summation
+  factsum <- sum(log(factorial(y)))
+  #second summation
+  lambdasum <- log(lambda) * sum(y)
+  #loglikelihood function
+  LL <- - (n * lambda) - factsum - lambdasum
+  return(LL)
+}
