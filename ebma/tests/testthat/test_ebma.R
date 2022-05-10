11121965005f044a16b6203f182e766fdb82a413
Alex Avery <a.d.avery@wustl.edu>
2022-05-10 12:31:36 -0500

    unit testing confusion

---
+++ ebma/tests/testthat/test_ebma.R
@@ -0,0 +1,4 @@
+test_that("z hat function returns correct calculations", {
+  expect_equal(#I dont know what to put here
+    #i know it is the object and then expected results but other than that I am lost.)
+})
---
+++ ebma/tests/testthat/test_z_hat.R
@@ -0,0 +1,5 @@
+
+test_that("z hat function returns correct calculations", {
+  expect_equal(#I dont know what to put here
+    #i know it is the object and then expected results but other than that I am lost.)
+})
